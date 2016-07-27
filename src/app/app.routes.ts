99caa516c41bbd92e3927400c97985c736df1baa
johnlindquist <johnlindquist@gmail.com>
2016-07-27 14:25:16 -0600

    second-route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import {Home} from "./home/home";
 import {provideRouter} from "@angular/router";
+import {Contacts} from "./contacts/contacts";
 const routes = [
-    {path: '', component: Home}
+    {path: '', component: Home},
+    {path: 'contacts', component: Contacts}
 ];
 
 export const APP_ROUTES = [
---
+++ src/app/contacts/contacts.ts
@@ -0,0 +1,6 @@
+import {Component} from '@angular/core';
+@Component({
+    selector: 'contacts',
+    template: `This is a contacts Component`
+})
+export class Contacts{}
