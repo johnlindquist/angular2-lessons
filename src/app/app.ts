edc2bc45f56cc959a45f406957d0da3d2f10029e
johnlindquist <johnlindquist@gmail.com>
2016-07-27 14:28:30 -0600

    routerLink

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,16 +4,19 @@ import {Router, ROUTER_DIRECTIVES} from "@angular/router";
 @Component({
     selector: 'app',
     directives: [ROUTER_DIRECTIVES],
-    template: `
-<!-- The address bar is for help viewing the URL in online demos-->
-  <div class="address-bar">
-  Online Demo Address Bar: <input     
-    (keyup.enter)="onEnter($event.target.value)"
-    [value]="router.url"
-    >
-  </div>
-  <hr>
-  <router-outlet></router-outlet>
+    template: `<!-- The address bar is for help viewing the URL in online demos-->
+<div class="address-bar">
+    Online Demo Address Bar: <input
+        (keyup.enter)="onEnter($event.target.value)"
+        [value]="router.url"
+>
+</div>
+<hr>
+<nav>
+    <a routerLink="">Home</a>
+    <a routerLink="contacts">Contacts</a>
+</nav>
+<router-outlet></router-outlet>
   `
 })
 export class App {
