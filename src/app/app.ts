354cdf052039dd17181ffe9ba66fa0bc2185940c
johnlindquist <johnlindquist@gmail.com>
2016-07-27 13:55:57 -0600

    basic-validation

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,6 @@ import {Component, ViewChild} from "@angular/core";
 
 @Component({
     selector: 'app',
-    styles: [`
-.ng-invalid{
-  border: 2px solid red;
-}
-.ng-valid{
-  border: 2px solid green;
-}
-`],
     template: `<form #f="ngForm" (ngSubmit)="onSubmit(f.value)">
   <fieldset ngModelGroup="login">
     <input required type="text" name="username" [ngModel]="firstName">
