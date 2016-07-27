c147fac61b22b49cd80d69f22eb5bc3ec192b2ae
johnlindquist <johnlindquist@gmail.com>
2016-07-27 12:56:54 -0600

    first-state

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,22 @@
 import {Component, trigger, state, style, transition, animate} from "@angular/core";
 
 @Component({
-  selector: 'app',
-
-  animations: [
-    trigger('signal', [
-      state('go', style({
-        'background-color': 'green'
-      })),
-      state('slow', style({
-        'background-color': 'yellow'
-      })),
-      state('stop', style({
-        'background-color': 'red'
-      }))
-    ])
-  ],
-  styles: [`
+    selector: 'app',
+    animations: [
+        trigger('signal', [
+            state('stop', style({
+                'background-color': 'red'
+            }))
+        ])
+    ],
+    styles: [`
     .light{
       width: 100px;
       height: 100px;
       background-color: green;
     }
 `],
-  template: `
+    template: `
 <div
   class="light"
   @signal="signal"
@@ -33,23 +26,13 @@ import {Component, trigger, state, style, transition, animate} from "@angular/co
 `
 })
 export class App {
-  signal = "go";
+    signal = "go";
 
-  update() {
-    this.signal = this.updateSignal(this.signal);
-  }
-
-  updateSignal(signal){
-    switch(signal){
-      case "go":
-        return "slow";
-      case "slow":
-        return "stop";
-      case "stop":
-        return "go";
-      default:
-        return "go";
+    update() {
+        this.signal =
+            this.signal == "go"
+                ? "stop"
+                : "go"
     }
-  }
 
 }
