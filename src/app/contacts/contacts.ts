494511d563502206ecde2c2312bf3c38c5d68cf4
johnlindquist <johnlindquist@gmail.com>
2016-07-27 15:01:28 -0600

    loading-contacts

--- src/app/contacts/contacts.ts
+++ src/app/contacts/contacts.ts
@@ -1,6 +1,18 @@
 import {Component} from '@angular/core';
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/map';
 @Component({
     selector: 'contacts',
-    template: `This is a contacts Component`
+    template: `
+<div *ngFor="let contact of contacts$ | async">
+{{contact.name}}
+</div>
+`
 })
-export class Contacts{}
+export class Contacts{
+    contacts$;
+    constructor(http:Http){
+        this.contacts$ = http.get(`https://starwars-json-server-aikiidixsl.now.sh/people`)
+            .map(res => res.json())
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {App} from './app/app';
 import {APP_ROUTES} from "./app/app.routes";
+import {HTTP_PROVIDERS} from "@angular/http";
 
-bootstrap(App, [APP_ROUTES])
+bootstrap(App, [APP_ROUTES, HTTP_PROVIDERS])
     .catch(err => console.log(err));
