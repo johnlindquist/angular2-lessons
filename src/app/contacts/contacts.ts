b5735e0a79ca67df1e9d50be0ff1c8ca3d317002
johnlindquist <johnlindquist@gmail.com>
2016-07-27 15:03:24 -0600

    routerLink-array-syntax

--- src/app/contacts/contacts.ts
+++ src/app/contacts/contacts.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {Http} from "@angular/http";
 import 'rxjs/add/operator/map';
+import {ROUTER_DIRECTIVES} from "@angular/router";
 @Component({
     selector: 'contacts',
+    directives:[ROUTER_DIRECTIVES],
     template: `
 <div *ngFor="let contact of contacts$ | async">
-{{contact.name}}
+    <a [routerLink]="['/contacts', contact.id]">{{contact.name}}</a>
 </div>
 `
 })
