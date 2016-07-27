098416964c0f94487551fd2fe70eda43c00626e8
johnlindquist <johnlindquist@gmail.com>
2016-07-27 13:52:46 -0600

    modeling-forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,53 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
     selector: 'app',
-    template: `
-<h1>{{message}}</h1>
-<input type="text" [(ngModel)]="message">
-`
+    styles: [`
+.ng-invalid{
+  border: 2px solid red;
+}
+.ng-valid{
+  border: 2px solid green;
+}
+`],
+    template: `<form #f="ngForm" (ngSubmit)="onSubmit(f.value)">
+  <fieldset ngModelGroup="login">
+    <input required type="text" name="username" [ngModel]="firstName">
+    <input required minlength="3" type="password" name="password" ngModel>
+  </fieldset>
+  
+  <fieldset ngModelGroup="vacation">
+  <label>Start Date:<input type="date" name="start" ngModel></label>
+  <label>End Date: <input type="date" name="end" ngModel></label>
+  
+  <label>Home<input type="radio" name="location" [ngModel]="location" value="home"></label>
+  <label>Away<input type="radio" name="location" [ngModel]="location" value="away"></label>
+</fieldset>
+  
+  <button type="submit">Submit</button>
+
+  <hr>
+  {{f.value | json}}
+  <hr>
+  {{f.valid}}
+
+
+</form>`
 })
 export class App {
-    message = `Hello, world!`
+    @ViewChild('f') f;
+
+    firstName = "John";
+    location = "away";
+
+    onSubmit(value) {
+        console.log(value);
+    }
+
+    ngAfterViewInit() {
+        this.f.valueChanges
+            .debounceTime(2000)
+            .subscribe(v => console.log(v));
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {App} from './app/app';
+import {provideForms, disableDeprecatedForms} from '@angular/forms';
 
-bootstrap(App);
+bootstrap(App, [
+    provideForms(),
+    disableDeprecatedForms()
+])
+    .catch(err => console.error(err));
