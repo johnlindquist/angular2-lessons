f6069d780b48662e5e8c276e59ce7b61974a0596
johnlindquist <johnlindquist@gmail.com>
2016-07-27 13:19:03 -0600

    hello-world

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,38 +1,12 @@
-import {Component, trigger, state, style, transition, animate} from "@angular/core";
+import {Component} from "@angular/core";
 
 @Component({
     selector: 'app',
-    animations: [
-        trigger('signal', [
-            state('stop', style({
-                'background-color': 'red'
-            }))
-        ])
-    ],
-    styles: [`
-    .light{
-      width: 100px;
-      height: 100px;
-      background-color: green;
-    }
-`],
     template: `
-<div
-  class="light"
-  @signal="signal"
-  (click)="update()"
-  ></div>
-  {{signal}}
+<h1>{{message}}</h1>
+<input type="text" [(ngModel)]="message">
 `
 })
 export class App {
-    signal = "go";
-
-    update() {
-        this.signal =
-            this.signal == "go"
-                ? "stop"
-                : "go"
-    }
-
+    message = `Hello, world!`
 }
