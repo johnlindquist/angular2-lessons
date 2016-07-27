452d841abf893c9a372a7af3e1b06629d5763a45
johnlindquist <johnlindquist@gmail.com>
2016-07-27 14:01:13 -0600

    first-form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import {Component, ViewChild} from "@angular/core";
+import {Component} from "@angular/core";
 
 @Component({
     selector: 'app',
     template: `<form #f="ngForm" (ngSubmit)="onSubmit(f.value)">
-  <fieldset ngModelGroup="login">
-    <input type="text" name="username" ngModel>
-    <input type="password" name="password" ngModel>
-  </fieldset>
+  <input type="text" name="username" ngModel>
+  <input type="password" name="password" ngModel>
   
   <button type="submit">Submit</button>
 
