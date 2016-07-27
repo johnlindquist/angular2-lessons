e9ec86d3a2d993669e5e119958e26348b299ca3c
johnlindquist <johnlindquist@gmail.com>
2016-07-27 14:00:15 -0600

    grouping-values

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import {Component, ViewChild} from "@angular/core";
     selector: 'app',
     template: `<form #f="ngForm" (ngSubmit)="onSubmit(f.value)">
   <fieldset ngModelGroup="login">
-    <input type="text" name="username" [ngModel]="firstName">
+    <input type="text" name="username" ngModel>
     <input type="password" name="password" ngModel>
   </fieldset>
   
@@ -16,8 +16,6 @@ import {Component, ViewChild} from "@angular/core";
 </form>`
 })
 export class App {
-    firstName = "John";
-
     onSubmit(value) {
         console.log(value);
     }
