ede291d9ce66db628a321f4a08644fc528bee0f9
johnlindquist <johnlindquist@gmail.com>
2016-07-27 10:49:21 -0600

    styles-within-animation

--- systemjs.config.js
+++ systemjs.config.js
@@ -7,7 +7,7 @@
 
     //map tells the System loader where to look for things
     var  map = {
-        'app':                        'app',
+        'app':                        'src',
 
         '@angular':                   'https://npmcdn.com/@angular', // sufficient if we didn't pin the version
         '@angular/router':            'https://npmcdn.com/@angular/router' + routerVer,
