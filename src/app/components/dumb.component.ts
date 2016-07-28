3e712082e7915a6c5690c0abfb847c74dc8e6f2b
johnlindquist <johnlindquist>
2016-07-28 20:03:37 +0000

    creating-elements-with-the-renderer

--- src/app/components/dumb.component.ts
+++ src/app/components/dumb.component.ts
@@ -1,40 +1,31 @@
-import {Component, Input, ViewChild, Renderer, ViewContainerRef, ComponentResolver} from "@angular/core";
-
-function componentBuilder(selector, template){
-    @Component({
-        selector, template
-    })
-    class SimpleComponent{}
-
-    return SimpleComponent;
-}
+import {Component, ViewChild, Renderer} from "@angular/core";
 
 @Component({
     selector: 'dumb',
-    styles: [`
-        :host{
-            font-family: Arial;
-            display: block;
-            border: 3px solid black;
-        }
+    styles:[`
+      :host{
+        font-family: Arial,serif;
+        display: block;
+        border: 3px solid black;
+      }
 `],
     template: `
-        <h3>Above</h3>
-        <div #putStuffHere></div>
-        <h3>Below</h3>
+      <h3>Above</h3>
+      <div #putStuffHere></div>
+      <h3>Below</h3>
 `
 })
-export class Dumb {
-    @ViewChild('putStuffHere', {read: ViewContainerRef}) putStuffHere;
+export class Dumb{
 
-    constructor(private resolver:ComponentResolver) {
-    }
+    @ViewChild('putStuffHere') putStuffHere;
 
-    ngAfterViewInit() {
-        this.resolver.resolveComponent(componentBuilder('whatever', `{{message}}`))
-            .then(factory => {
-                const ref = this.putStuffHere.createComponent(factory);
-                ref.instance.message = "Awesome";
-            });
+    constructor(public renderer:Renderer){}
+
+    ngAfterViewInit(){
+        const div = this.renderer.createElement(this.putStuffHere.nativeElement, 'div', null);
+        this.renderer.setElementProperty(div, 'innerText', 'Wow');
+        const input = this.renderer.createElement(div, 'input', null);
+        this.renderer.setElementProperty(input, 'value', 'neat');
+
     }
 }
