0010da18ec67a2a14b5de7f7cdb8eb35a0875593
johnlindquist <johnlindquist>
2016-08-18 19:18:28 +0000

    hello-world

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,11 @@
 import {Component} from "@angular/core";
 @Component({
     selector: 'app',
-    template: `My amazing template`
+    template: `
+<input type="text" [(ngModel)]="message">
+<div>{{message}}</div>
+`
 })
 export class AppComponent {
+    message = "Hello"
 }
