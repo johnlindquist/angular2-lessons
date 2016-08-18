9f4792329f0ffa29de9bcf4c34c30b2293e72102
johnlindquist <johnlindquist>
2016-08-18 21:07:23 +0000

    simple-login-form

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,30 @@
 import {Component} from "@angular/core";
+import {FormGroup, FormControl} from "@angular/forms";
 @Component({
     selector: 'app',
     template: `
-<input type="text" [(ngModel)]="message">
-<div>{{message}}</div>
+<form [formGroup]="loginForm" (ngSubmit)="onLogIn(loginForm.value)">
+    <label><input type="text" formControlName="username"></label>
+    <label><input type="password" formControlName="password"></label>
+    <button type="submit">Submit</button>
+</form>
 `
 })
 export class AppComponent {
-    message = "Hello"
+    onLogIn(formValue){
+        console.log(formValue);
+    }
+
+    loginForm = new FormGroup({
+        username: new FormControl(),
+        password: new FormControl()
+    });
+
+    ngOnInit(){
+        this.loginForm.setValue({username: 'john', password: 'password'});
+    }
+
+    ngAfterViewInit(){
+        this.loginForm.valueChanges.subscribe(v => console.log(v));
+    }
 }
