ff65f420ffeb6189210dd9df4f584b742b5695c2
johnlindquist <johnlindquist>
2016-08-22 22:22:41 +0000

    grouping-values

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,17 +6,20 @@ import {Component, ViewChild} from "@angular/core";
     #formRef="ngForm" 
     (ngSubmit)="onSubmit(formRef.value)"
     >
-    <input 
-        #usernameRef="ngModel"
-        name="username"
-        [(ngModel)]="username"
-        type="text"        
-        required
-        minlength="3"
-    >    
-    <div *ngIf="usernameRef.errors?.required">This field is required</div>
-    <div *ngIf="usernameRef.errors?.minlength">This field must be longer than {{usernameRef.errors?.minlength.requiredLength}} characters. You only typed {{usernameRef.errors?.minlength.actualLength}}</div>
-    
+    <fieldset ngModelGroup="login">
+        <input 
+            #usernameRef="ngModel"
+            name="username"
+            [(ngModel)]="username"
+            type="text"        
+            required
+            minlength="3"
+        >    
+        <div *ngIf="usernameRef.errors?.required">This field is required</div>
+        <div *ngIf="usernameRef.errors?.minlength">This field must be longer than {{usernameRef.errors?.minlength.requiredLength}} characters. You only typed {{usernameRef.errors?.minlength.actualLength}}</div>
+        
+        <input type="password" ngModel name="password">
+    </fieldset>
     <button type="submit">Submit</button>
 </form> 
 {{formRef.value | json}}   
