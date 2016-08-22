a5352913e2051f38d84909708df7a3c8f9da3364
johnlindquist <johnlindquist>
2016-08-22 21:50:38 +0000

    ngmodel-error-messaging

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,16 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
 @Component({
     selector: 'app',
     template: `
-    <input required type="text" 
-    [(ngModel)]="username"
-    #usernameRef="ngModel"
-    >
-    {{usernameRef.valid}}
+    <input 
+        type="text" 
+        [(ngModel)]="username"
+        #usernameRef="ngModel"
+        required
+        minlength="3"
+    >    
+    <div *ngIf="usernameRef.errors?.required">This field is required</div>
+    <div *ngIf="usernameRef.errors?.minlength">This field must be longer than {{usernameRef.errors?.minlength.requiredLength}} characters. You only typed {{usernameRef.errors?.minlength.actualLength}}</div>
 `
 })
 export class AppComponent {
