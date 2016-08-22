3d3958a4195054e7310fe8e59c32237ca280c204
johnlindquist <johnlindquist>
2016-08-22 22:17:16 +0000

    basic-ngform

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,17 +2,32 @@ import {Component, ViewChild} from "@angular/core";
 @Component({
     selector: 'app',
     template: `
+<form 
+    #formRef="ngForm" 
+    (ngSubmit)="onSubmit(formRef.value)"
+    >
     <input 
-        type="text" 
-        [(ngModel)]="username"
         #usernameRef="ngModel"
+        name="username"
+        [(ngModel)]="username"
+        type="text"        
         required
         minlength="3"
     >    
     <div *ngIf="usernameRef.errors?.required">This field is required</div>
     <div *ngIf="usernameRef.errors?.minlength">This field must be longer than {{usernameRef.errors?.minlength.requiredLength}} characters. You only typed {{usernameRef.errors?.minlength.actualLength}}</div>
+    
+    <button type="submit">Submit</button>
+</form> 
+{{formRef.value | json}}   
+{{formRef.valid | json}}   
+   
 `
 })
 export class AppComponent {
     username = "John";
+
+    onSubmit(formValue){
+        console.log(formValue);
+    }
 }
