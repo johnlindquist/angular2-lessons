bc28aa12fa8cffa3251e1162b07e793375e7e37e
johnlindquist <johnlindquist>
2016-08-23 02:34:20 +0000

    combining-input-validators

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,7 @@ import {Component, ViewChild} from "@angular/core";
     selector: 'app',
     template: `
 <form>
-
+    <h2>Both fields combined need to be > 10</h2>
     <input type="number" name="a" [(ngModel)]="a">
     <input type="number" name="b" [(ngModel)]="b">
     <input 
@@ -14,16 +14,19 @@ import {Component, ViewChild} from "@angular/core";
         [ngModel]="a + b"
         >
     <hr>        
-    A is {{a}}, B is {{b}}, so combined they are {{total.valid ? 'valid' : 'invalid'}}
+    A is {{a}}, B is {{b}}, so combined they are <h3>{{total.valid ? 'valid' : 'invalid'}}</h3>
 </form>
 `
 })
 export class AppComponent {
+    a = 3;
+    b = 4;
+
     @ViewChild('total') total;
 
     ngAfterViewInit(){
         this.total.control.setValidators([
-           control => control.value > 10
+           control => control.value <= 10
                    ? {valid: false}
                    : null
         ]);
