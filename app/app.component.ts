a7a398bec854cbc0d95f2c6fd2eb97d13f0c3233
johnlindquist <johnlindquist>
2016-08-23 02:30:49 +0000

    combining-input-validators

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,55 +1,31 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
+
 @Component({
     selector: 'app',
-    styles:[`
-.ng-invalid{
-    border: 3px solid red;
-}
-.ng-valid{
-    border: 3px solid green;
-}
-`],
     template: `
-<form 
-    #formRef="ngForm" 
-    (ngSubmit)="onSubmit(formRef.value)"
-    >
-    <fieldset ngModelGroup="login">
-        <input 
-            #usernameRef="ngModel"
-            name="username"
-            [(ngModel)]="username"
-            type="text"        
-            required
-            minlength="3"
-        >    
-        <div *ngIf="usernameRef.errors?.required">This field is required</div>
-        <div *ngIf="usernameRef.errors?.minlength">This field must be longer than {{usernameRef.errors?.minlength.requiredLength}} characters. You only typed {{usernameRef.errors?.minlength.actualLength}}</div>
-        
-        <hr>
-        Pristine: {{usernameRef.pristine}}
-        <hr>
-        Dirty: {{usernameRef.dirty}}
-        <hr>
-        Untouched: {{usernameRef.untouched}}
-        <hr>
-        Touched: {{usernameRef.touched}}        
-        <hr>
-        
-        
-        <input type="password" ngModel name="password">
-    </fieldset>
-    <button type="submit">Submit</button>
-</form> 
-{{formRef.value | json}}   
-{{formRef.valid | json}}   
-   
+<form>
+
+    <input type="number" name="a" [(ngModel)]="a">
+    <input type="number" name="b" [(ngModel)]="b">
+    <input 
+        type="hidden" 
+        name="total" 
+        #total="ngModel"
+        [ngModel]="a + b"
+        >
+    <hr>        
+    A is {{a}}, B is {{b}}, so combined they are {{total.valid ? 'valid' : 'invalid'}}
+</form>
 `
 })
 export class AppComponent {
-    username = "John";
+    @ViewChild('total') total;
 
-    onSubmit(formValue){
-        console.log(formValue);
+    ngAfterViewInit(){
+        this.total.control.setValidators([
+           control => control.value > 10
+                   ? {valid: false}
+                   : null
+        ]);
     }
 }
