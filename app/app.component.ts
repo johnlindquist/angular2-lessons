0f8f0663e01d3215adc4c7087c9cf35dab219479
johnlindquist <johnlindquist>
2016-08-24 16:14:38 +0000

    select-dropdown

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,11 +2,6 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: 'app',
-    styles:[`
-.ng-invalid + label:after{
-    content: '<--Pick one!!!'
-}
-`],
     template: `
 <form #formRef="ngForm">
     <select name="location" [ngModel]="locations[0]">
