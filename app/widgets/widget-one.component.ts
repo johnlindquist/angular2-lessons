e06bd0b8a65ae66202281f1235c96c54279ab437
johnlindquist <johnlindquist>
2016-08-30 21:28:17 +0000

    styling-components

--- app/widgets/widget-one.component.ts
+++ app/widgets/widget-one.component.ts
@@ -1,6 +1,13 @@
 import {Component, Input} from "@angular/core";
 @Component({
     selector: 'widget-one',
+    styles:[`
+:host{
+    display: block;
+    border: 3px dashed black;
+    font-family: "Times New Roman";
+}
+`],
     template: `
 <h2>One's message:</h2>
 <h3>{{message}}</h3>
