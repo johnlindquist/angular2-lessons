5e936ebf81e6d20921c0be646969b219b1eef20b
johnlindquist <johnlindquist>
2016-08-30 19:08:01 +0000

    app-component

--- app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from "@angular/core";
-@Component({
-    selector:'app',
-    template: `
-<div>I'm the App Component</div>
-`
-})
-export class AppComponent{}
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,19 @@
+import {Component, NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
-import {AppModule} from "./app.module";
+@Component({
+    selector:'app',
+    template: `
+<div>I'm the App Component</div>
+`
+})
+export class AppComponent{}
 
+@NgModule({
+    imports:[BrowserModule],
+    declarations:[AppComponent],
+    bootstrap:[AppComponent]
+})
+export class AppModule{}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
