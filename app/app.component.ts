afeb6d959979d824c52ab96b7c4f33dc18922086
johnlindquist <johnlindquist>
2016-09-07 21:58:51 +0000

    navigating-with-routerlink

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,10 @@ import {Component} from "@angular/core";
 @Component({
     selector:'app',
     template: `
+<nav>
+    <a routerLink="">Home</a>
+    <a routerLink="contacts">Contacts</a>
+</nav>
 <router-outlet></router-outlet>
 `
 })
