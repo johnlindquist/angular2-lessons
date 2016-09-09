ae699e95f5e290620d4fead0474fe3f54bcfea0f
johnlindquist <johnlindquist>
2016-09-09 15:13:46 +0000

    first-state

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,67 +1,27 @@
-import {Component, trigger, state, style, transition, animate, keyframes} from "@angular/core";
+import {Component, trigger, state, style} from "@angular/core";
 @Component({
-    selector:'app',
+    selector: 'app',
     animations:[
-        trigger('trafficLight', [
-            state('stop', style({
-                backgroundColor: 'red'
-            })),
-            state('slow', style({
-                backgroundColor: 'yellow'
-            })),
+        trigger('signal', [
             state('go', style({
-                backgroundColor: 'green'
-            })),
-            transition('* => *', animate(3000, keyframes([
-                style({transform: 'translate(0px, 0px)'}),
-                style({transform: 'translateX(100px)'}),
-                style({transform: 'translateY(100px)'}),
-                style({transform: 'translate(0px, 0px)'})
-
-            ])))
-
+                'background-color':'green'
+            }))
         ])
     ],
     styles:[`
 .traffic-light{
     width: 100px;
     height: 100px;
-    background-color: green;
+    background-color: black;
 }
 `],
     template: `
-<div 
-    class="traffic-light"
-    [@trafficLight]="signal"
-    (@trafficLight.start)="onStart($event)"
-    (@trafficLight.done)="onDone($event)"
-    >    
+<div
+    [@signal]="'go'"
+    class="traffic-light">
+    
 </div>
-<button (click)="onStopClick()">Stop</button>
-<button (click)="onSlowClick()">Slow</button>
-<button (click)="onGoClick()">Go</button>
 `
 })
-export class AppComponent{
-    signal:'go'|'stop'|'slow' = 'go';
-
-    onStopClick(){
-        this.signal = 'stop';
-    }
-
-    onSlowClick(){
-        this.signal = 'slow';
-    }
-
-    onGoClick(){
-        this.signal = 'go';
-    }
-
-    onStart(foo){
-        console.log(foo);
-    }
-
-    onDone(event){
-        console.log(event);
-    }
+export class AppComponent {
 }
