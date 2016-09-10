418f66268edd492fc6b301ded889a8c60c1ffffb
johnlindquist <johnlindquist>
2016-09-10 14:49:30 +0000

    delay-and-easing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, trigger, state, style, transition, animate, keyframes} from "@angular/core";
+import {Component, trigger, state, style, transition, animate} from "@angular/core";
 @Component({
     selector: 'app',
     animations:[
@@ -14,13 +14,7 @@ import {Component, trigger, state, style, transition, animate, keyframes} from "
                 'background-color':'red',
                 'height':'50px'
             })),
-            transition('void => *', animate(8000, keyframes([
-                style({'transform':'scale(0)'}),
-                style({'transform':'scale(.1)'}),
-                style({'transform':'scale(.9)'}),
-                style({'transform':'scale(1)'})
-            ]))),
-            transition('* => *', animate('2s 1s cubic-bezier(0.175, 0.885, 0.32, 1.275)'))
+            transition('* => *', animate('2s 1s cubic-bezier(0.455, 0.03, 0.515, 0.955)'))
         ])
     ],
     styles:[`
