cef2336b639d71189f27342cc4f20b731a9e7b01
John Lindquist <johnlindquist@gmail.com>
2016-09-29 15:40:36 -0600

    optimize-requests

--- app/contacts/contact.component.ts
+++ app/contacts/contact.component.ts
@@ -1,27 +1,27 @@
-import {Component} from "@angular/core";
-import {ActivatedRoute} from "@angular/router";
+import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/switchMap";
-import "rxjs/add/operator/startWith";
-import {Http} from "@angular/http";
+import { Http } from "@angular/http";
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Component({
-    template:`
+    template: `
 <h2>{{(contact$ | async).name}}</h2>
 <img [src]="(contact$ | async).image">
 `
 })
-export class ContactComponent{
-    contact$;
+export class ContactComponent {
+    contact$ = new BehaviorSubject({name: 'Loading...', image: ''});
 
-    constructor(private route:ActivatedRoute, private http:Http){
+    constructor(private route: ActivatedRoute, private http: Http) {
         const api = 'https://starwars-json-server-ewtdxbyfdz.now.sh/';
 
-        this.contact$ = route.params
-            .map((p:any) => p.id)
+        route.params
+            .map((p: any) => p.id)
             .switchMap(id => http.get(api + 'people/' + id)
                 .map(res => res.json())
                 .map(contact => Object.assign({}, contact, {image: api + contact.image}))
             )
-            .startWith({name: '', image: ''})
+            .subscribe(this.contact$);
     }
 }
